eb5008ccac778d1dcce804b1ce652c32daa76864
oscar cervantes <oscarcr189@outlook.com>
2023-04-18 23:20:48 -0600

    actualizacion39

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -5,10 +5,24 @@
 
     $db = conectarDB();
 
+//arreglo con mensaje de errores
+
+    $errores=[];
+
+    $titulo = "";
+    $precio = "";
+    $descripcion = "";
+    $habitaciones = "";
+    $wc = "";
+    $estacionamiento = "";
+    $vendedores_id = "";
+//ejecutarel codigo despues de que el usuario envia formulario 
+
+
     if ($_SERVER['REQUEST_METHOD'] == 'POST'){
-        echo "<pre>";
-        var_dump($_POST);
-        echo "</pre>";
+        // echo "<pre>";
+        // var_dump($_POST);
+        // echo "</pre>";
 
         $titulo = $_POST['titulo'];
         $precio = $_POST['precio'];
@@ -18,17 +32,58 @@
         $estacionamiento = $_POST['estacionamiento'];
         $vendedores_id = $_POST['vendedores_id'];
 
-        //insertar en la base de datos
+        if(!$titulo){
+            $errores[] ="Debes añadir un titulo";
 
-        $query = "INSERT INTO propiedades (titulo, precio,descripcion, habitaciones, wc, estacionamiento, vendedores_id ) VALUES ('$titulo','$precio','$descripcion','$habitaciones','$wc','$estacionamiento','$vendedores_id ')";
-
-        // echo "$query";
-
-        $resultado = mysqli_query($db, $query);
-
-        if ($resultado){
-            echo "Insertado correctamente";
         }
+
+        if(!$precio){
+            $errores[]="El precio es obligatorio";
+        }
+
+        if(strlen($descripcion)<50){
+            $errores[]="La descripcion es obligatorio y debe tener al menos 50 caracteres";
+        }
+
+        if(!$habitaciones){
+            $errores[]="El numero de habitaciones es obligatorio";
+        }
+
+        if(!$wc){
+            $errores[]="El numero de baños es obligatorio";
+        }
+
+        if(!$estacionamiento){
+            $errores[]="El numero de estacionamientos es obligatorio";
+        }
+
+        if(!$vendedores_id){
+            $errores[]="Elige un vendedor";
+        }
+        // echo "<pre>";
+        // var_dump($errores);
+        // echo "</pre>";
+
+        // exit;
+
+        //revisar que el array de errores este vacio
+        if (empty($errores)){
+            //insertar en la base de datos
+            $query = "INSERT INTO propiedades (titulo, precio,descripcion, habitaciones, wc, estacionamiento, vendedores_id ) VALUES ('$titulo',
+            '$precio','$descripcion','$habitaciones','$wc','$estacionamiento','$vendedores_id ')";
+
+            // echo "$query";
+
+            $resultado = mysqli_query($db, $query);
+
+            if ($resultado){
+                echo "Insertado correctamente";
+            }
+
+        }
+
+
+
     }
     
 
@@ -44,6 +99,13 @@
        
         <a href="../index.php" class="boton boton-verde">volver</a>
 
+        <?php foreach($errores as $errores): ?>
+
+        <div class="alerta error">
+            <?php echo $errores; ?>            
+        </div>
+            
+        <?php endforeach; ?>
         <br>
 
         <form class="formulario" method="POST" action="../propiedades/crear.php">
@@ -52,10 +114,10 @@
                 <legend>Informacion General</legend>
                 
                 <label for="titulo">Titulo:</label>
-                <input type="text" id="titulo" name="titulo " placeholder="Titulo Propiedad">
+                <input type="text" id="titulo" name="titulo " placeholder="Titulo Propiedad" value="<?php echo $titulo; ?>">
                 
                 <label for="precio">Precio:</label>
-                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
+                <input type="number" id="precio" name="precio" placeholder="Precio Propiedad" value="<?php echo $precio; ?>">
 
                 <label for="imagen">Imagen:</label>
                 <input type="file" id="imagen" accept="image/jpeg, image/png">
@@ -82,6 +144,7 @@
                 <legend>Vendedor</legend>
                 <select name="vendedor" id="">
 
+                    <option value=""> -- Seleccione --</option>
                     <option value="1">Juan </option>
                     <option value="2">Karen </option>
 
